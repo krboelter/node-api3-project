e4511499efba8ee978efdd916d60e5baa8b7705a
krboelter <krboelter@gmail.com>
2019-12-11 16:49:17 -0700

    almost finished validation middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const db = require("./userDb")
+const userDb = require("./userDb")
+const postDb = require("../posts/postDb")
+
 
 const router = express.Router();
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validatePost(), (req, res) => {
+  res.status(200).json(req.newPost)
 });
 
 router.get('/', (req, res) => {
-  db.get()
+  userDb.get()
     .then(users => {
       return res.status(200).json(users)
     })
     .catch(err => {
-      return res.status(500).json({ message: "Cannot reach page."})
+      return res.status(500).json({ message: "Cannot reach page." })
     })
 });
 
@@ -21,8 +23,11 @@ router.get('/:id',validateUserId(), (req, res) => {
   res.status(200).json(req.user)
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts',validateUserId(), (req, res) => {
+  res.status(200).json({ 
+    message: `${req.user.name} posted:`,
+    posts: req.posts
+  })
 });
 
 router.delete('/:id', (req, res) => {
@@ -37,13 +42,19 @@ router.put('/:id', (req, res) => {
 
 function validateUserId() {
   return (req, res, next)  => {
-    db.getById()
+    const id = req.params.id
+
+    userDb.getById(id)
       .then(user => {
-        req.user = user
-        next()
+        if (!user) {
+          res.status(400).json({ message: "Invalid user id." })
+        } else {
+          req.user = user
+          next()
+        }
       })
       .catch(err => {
-        json.sttaus(500).json({ message: "Internal server error..."})
+        json.sttaus(500).json({ message: "Internal server error..." })
       })
   }
 }
@@ -52,8 +63,23 @@ function validateUser(req, res, next) {
   // do your magic!
 }
 
-function validatePost(req, res, next) {
-  // do your magic!
+function validatePost() {
+  return (req, res, next) => {
+    const id = req.params.id
+    const post = {
+      text: req.body.text,
+      user_id: id
+    }
+
+    postDb.insert(post)
+      .then(newPost => {
+        req.newPost = newPost
+        next()
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Internal server error..." })
+      })
+  }
 }
 
 module.exports = router;
